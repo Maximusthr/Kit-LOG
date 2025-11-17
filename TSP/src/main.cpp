4c23fc7ac3b3e3fcb59094e6e50d089856e534cd
Arthur Maximus <arthurmcb321@gmail.com>
2025-11-17 00:29:52 -0300

    Add: Construcao

--- TSP/src/main.cpp
+++ TSP/src/main.cpp
@@ -1,10 +1,117 @@
 #include "Data.h"
-// #include <iostream>
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#include <set>
+#include <vector>
 
 using namespace std;
+using ll = long long;
 
-/*
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int INF = (int)1e9;
+const ll LINF = (ll)1e18;
+
+struct Solution {
+    vector<int> sequence;
+    double cost = 0.0;
+};
+
+struct InsertionInfo{
+    int noInserido;
+    int arestaRemovida;
+    double custo;
+};
+
+int n;
+vector<vector<double>> g;
+
+vector<InsertionInfo> calcularCustoInsercao(Solution &s, set<int> &CL){
+    vector<InsertionInfo> custoInsercao((s.sequence.size() - 1) * CL.size());
+
+    int l = 0;
+    for (int a = 0; a < s.sequence.size() - 1; a++){
+        int i = s.sequence[a];
+        int j = s.sequence[a+1];
+        for (auto k : CL){
+            custoInsercao[l].custo = g[i][k] + g[j][k] - g[i][j];
+            custoInsercao[l].noInserido = k;
+            custoInsercao[l].arestaRemovida = a;
+            l++;
+        }
+    }
+
+    return custoInsercao;
+}
+
+void escolher3NosAleatorios(Solution &s, vector<bool> &elementos){
+    uniform_int_distribution<int> dist(2, n);
+
+    int achou = 0;
+    while (achou != 3){
+        int valor = dist(rng);
+
+        if (!elementos[valor]){
+            achou++;
+            elementos[valor] = true;
+            s.sequence.push_back(valor);
+        }
+    }
+}
+
+void nosRestantes(set<int> &CL, vector<bool> &elementos){
+    for (int i = 1; i <= n; i++){
+        if (!elementos[i]) CL.insert(i);
+    }
+}
+
+void inserirNaSolucao(Solution &s, vector<InsertionInfo> &no, int selecionado){
+    s.cost += no[selecionado].custo;
+    
+    s.sequence.insert(s.sequence.begin() + no[selecionado].arestaRemovida + 1, no[selecionado].noInserido);
+}
+
+Solution Construcao(){
+    Solution s;
+    
+    s.cost = 0;
+
+    vector<bool> elementos(n+1);
+    elementos[1] = true;
+    
+    s.sequence.push_back(1);
+    escolher3NosAleatorios(s, elementos);
+    s.sequence.push_back(1);
+
+    for (int i = 0; i < s.sequence.size() - 1; i++){
+        s.cost += g[s.sequence[i]][s.sequence[i+1]];
+    }
+    
+    set<int> CL;
+    nosRestantes(CL, elementos);
+   
+    while (!CL.empty()){
+        vector<InsertionInfo> custoInsercao = calcularCustoInsercao(s, CL);
+
+        sort(custoInsercao.begin(), custoInsercao.end(), [&](InsertionInfo x, InsertionInfo y){
+            return x.custo < y.custo;
+        });
+
+        double alpha = (double) rand() / RAND_MAX;
+
+        int selecionado = rand() % ((int) ceil (alpha * (custoInsercao.size() - 1)));
+
+        inserirNaSolucao(s, custoInsercao, selecionado);
+
+        CL.erase(custoInsercao[selecionado].noInserido);
+    }
+
+    return s;
+}
+
+
 Solution ILS(int maxIter, int maxIterIls){
     Solution bestOfAll;
 
@@ -13,105 +120,68 @@ Solution ILS(int maxIter, int maxIterIls){
         Solution s = Construcao();
         Solution best = s;
 
-        int iterILS = 0;
+        // int iterILS = 0;
 
-        while(iterIls <= maxIterIls){
-            BuscaLocal (&s);
+        // while(iterIls <= maxIterIls){
+        //     BuscaLocal (&s);
 
-            if (s.cost < best.cost){
-                best = s;
-                iterIls = 0;
-            }
+        //     if (s.cost < best.cost){
+        //         best = s;
+        //         iterIls = 0;
+        //     }
             
-            s = Perturbacao(best);
+        //     s = Perturbacao(best);
 
-            iterIls++;
-        }
+        //     iterIls++;
+        // }
         
         if (best.cost < bestOfAll.cost){
             bestOfAll = best;
         }
     }
+
+    return bestOfAll;
 }
-*/
-
-/*
-CONSTRUCAO():
-
-
-*/
-
-// struct InsertionInfo{
-//     int noInserido;
-//     int arestaRemovida;
-//     double custo;
-// };
-
-// vector<InsertionInfo> calcularCustoInsercao(Solution &s, vector<int> &CL){
-//     vector<InsertionInfo> custoInsercao = vector<InsertionInfo> ((s.size() -1) * CL.size());
-
-//     int l = 0;
-//     for (int a = 0; a < s.sequence.size() -1; a++){
-//         int i = s.sequence[a];
-//         int j = s.sequence[a+1];
-//         for (auto k : CL){
-//             custoInsercao[l].custo = c[i][k] + c[j][k] - c[i][j];
-//             custoInsercao[l].noInserido = k;
-//             custoInsercao[l].arestaRemovida = a;
-//             l++;
-//         }
-//     }
-
-//     return custoInsercao;
-// }
-
-// Solution Construcao(){
-//     Solution s;
-//     s.sequence = escolher3NosAleatorios();
-//     vector<int> CL = nosRestantes(); 
-//     /*
-//     ex: V = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-//     s.sequence = {1, 2, 9, 5, 1}
-//     CL = {3, 4, 6, 7, 8, 10}
-//     */
-
-//     while (!CL.empty()){
-//         vector<InsertionInfo> custoInsercao = calcularCustoInsercao(s, CL);
-
-//         ordenarEmOrdemCrescente(custoInsercao);
-
-//         double alpha (double) rand / RAND_MAX;
-
-//         int selecionado = rand() % ((int) ceil (alpha * custoInsercao.size()));
-
-//         inserirNaSolucao(s, custoInsercao[selecionado].noInserido);
-//     }
-
-//     return s;
-// }
-
 
 
 int main(int argc, char** argv) {
 
     auto data = Data(argc, argv[1]);
     data.read();
-    size_t n = data.getDimension();
+    n = data.getDimension();
 
-    cout << "Dimension: " << n << endl;
-    cout << "DistanceMatrix: " << endl;
-    data.printMatrixDist();
+    g.resize(n + 1, vector<double> (n + 1));
 
-
-    cout << "Exemplo de Solucao s = ";
-    double cost = 0.0;
-    for (size_t i = 1; i < n; i++) {
-        cout << i << " -> ";
-        cost += data.getDistance(i, i+1);
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            g[i][j] = data.getDistance(i, j);
+        }
     }
-    cost += data.getDistance(n, 1);
-    cout << n << " -> " << 1 << endl;
-    cout << "Custo de S: " << cost << endl;
+
+    int maxIter = 50;
+    int maxIterILS = (n/2 ? n >= 150 : n);
+
+    Solution fim = ILS(maxIter, maxIterILS);
+
+    cout << fim.cost << "\n";
+    for (int i = 0; i < fim.sequence.size(); i++){
+        cout << fim.sequence[i] << " \n"[i == fim.sequence.size() - 1];
+    }
+
+    // cout << "Dimension: " << n << endl;
+    // cout << "DistanceMatrix: " << endl;
+    // data.printMatrixDist();
+
+
+    // cout << "Exemplo de Solucao s = ";
+    // double cost = 0.0;
+    // for (size_t i = 1; i < n; i++) {
+    //     cout << i << " -> ";
+    //     cost += data.getDistance(i, i+1);
+    // }
+    // cost += data.getDistance(n, 1);
+    // cout << n << " -> " << 1 << endl;
+    // cout << "Custo de S: " << cost << endl;
 
     return 0;
 }
